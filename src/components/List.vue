e2829dc23f39a8acaba4c7291ae80e0db5d524e0
Kevin Nevile <kevin.nevileO@gmail.comm>
2023-09-07 14:06:44 -0300

    formating

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+
+</script>
+
+<template>  
+    <div class="col-4">
+        <div class="card mb-3">
+        <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body mx-auto">
+                <h5 class="card-title text-center capitalize">Teste</h5>
+
+                <a href="..." target="_blank" class="btn btn-primary">Detalhes</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style> 
+    .capitalize{
+        text-transform: capitalize;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">teste</h5>
+              <p class="card-text">teste</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+
+        </div>
+      </div>
+    </div>
   </main>
 </template>
