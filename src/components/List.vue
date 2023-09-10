d966d0f5f93db5a51a3553870189ad06cdf8777a
Kevin Nevile <kevin.nevileO@gmail.comm>
2023-09-10 20:01:26 -0300

    descriçao raça

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,15 @@
 <script setup>
-const race = defineProps(["name", "url"])
+const race = defineProps(["name", "url", "alig"])
 
 </script>
 
 <template>  
     <div class="col-3 mt-5 ">
         <div class="card mb-3">
-        <img class="card-img-top" src="../assets/favicon.ico" alt="Card image cap">
+        <img class="card-img-top" src="..." alt="Card image cap">
             <div class="card-body mx-auto">
                 <h5 class="card-title text-center capitalize">{{ race.name }}</h5>
+                <p class="card-text">{{ race.alig }}</p>
                 <a :href="'https://www.dnd5eapi.co/api/races/' + race.url" target="_blank" class="btn btn-primary">Detalhes</a>
             </div>
         </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,56 +16,6 @@ onMounted(() => {
 </script>
 
 <template>
-
-  <!-- <main>
-    <div class="container">
-      <div class="row mt-4" style="background-color: brown;">
-        <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="../assets/favicon.ico" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">teste</h5>
-              <p class="card-text">teste</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-10 col-md-4"></div>
-          <div class="card">
-            <div class="card-body row">
-              <List
-              v-for="classe in classes"
-              :key="classe.name"
-              :name="classe.name"
-              :url="classe.url"
-              />
-            </div>
-          </div>
-      </div>
-    </div>
-  </main> -->
-
-  <!-- <main>
-      <div class="row mx-auto">
-        <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="../assets/favicon.ico" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">teste</h5>
-              <p class="card-text">teste</p>
-            </div>
-          </div>
-        </div>
-            <div class="card-body row mx-auto">
-              <List
-              v-for="classe in classes"
-              :key="classe.name"
-              :name="classe.name"
-              :url="classe.url"
-              />
-            </div>
-      </div>
-  </main> -->
-
   <main>
     <div class="container mb-5">
       <div class="row mx-auto">
@@ -84,11 +34,11 @@ onMounted(() => {
               :key="race.name"
               :name="race.name"
               :url="race.index"
+              :alig="race.alignment"
               />
             </div>
       </div>
     </div>
   </main>
-
 </template>
 
