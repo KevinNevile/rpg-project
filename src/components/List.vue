14947f601942b5af794f1831528d3c691915fbf7
Kevin Nevile <kevin.nevileO@gmail.comm>
2023-09-21 20:32:31 -0300

    Add Modal

--- src/components/List.vue
+++ src/components/List.vue
@@ -11,10 +11,30 @@ const imageUrl = `../src/assets/races/${race.url}.png`
             <div class="card-body mx-auto">
                 <h5 class="card-title text-center capitalize">{{ race.name }}</h5>
                 <p class="card-text">{{ race.alig }}</p>
-                <a :href="'https://www.dnd5eapi.co/api/races/' + race.url" target="_blank" class="btn btn-primary">Detalhes</a>
+                <a :href="'https://www.dnd5eapi.co/api/races/' + race.url" target="_blank" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Detalhes</a>
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Detalhes sobre a Raça</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
 </template>
 
 <style> 
